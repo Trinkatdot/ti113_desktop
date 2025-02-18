83bc194bad24529bde901142498b7fca6e0a86a6
Trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2025-02-18 20:00:37 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro_Programa/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Primeiro_Programa
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            // Leitura das notas
+            Console.Write("Digite a primeira nota: ");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota: ");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            // Cálculo da média
+            double media = (nota1 + nota2) / 2;
+
+            // Exibição do resultado com base na média
+            Console.WriteLine($"Média: {media:F2}");
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if (media < 5)
+            {
+                Console.WriteLine("Reprovado");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação");
+            }
+        }
+    }
+
+}
---
+++ Segundo_Programa/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Segundo_Programa
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            // Solicita ao usuário um número de 1 a 7
+            Console.Write("Digite um número de 1 a 7: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            // Determina o dia da semana correspondente
+            string diaSemana = numero switch
+            {
+                1 => "Domingo",
+                2 => "Segunda-feira",
+                3 => "Terça-feira",
+                4 => "Quarta-feira",
+                5 => "Quinta-feira",
+                6 => "Sexta-feira",
+                7 => "Sábado",
+                _ => "Número inválido! Por favor, insira um número entre 1 e 7."
+            };
+
+            // Exibe o resultado
+            Console.WriteLine(diaSemana);
+        }
+    }
+}

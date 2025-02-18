f0a663b857f774730ac9eed57af0b3090c42739f
Trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2025-02-18 20:12:14 -0300

    Feat: exercicio 1 ao 3

---
+++ Terceiro_programa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Terceiro_programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
